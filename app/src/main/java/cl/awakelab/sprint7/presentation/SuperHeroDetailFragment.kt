e7b69eca49f459a7b61d899d891600cb53943812
Claudio <claudioan00@gmail.com>
2023-08-31 21:11:54 -0400

    Fragmento detalle avanzado

--- app/src/main/java/cl/awakelab/sprint7/presentation/SuperHeroDetailFragment.kt
+++ app/src/main/java/cl/awakelab/sprint7/presentation/SuperHeroDetailFragment.kt
@@ -1,11 +1,18 @@
 package cl.awakelab.sprint7.presentation
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import cl.awakelab.sprint7.R
+import cl.awakelab.sprint7.data.local.entities.SuperHeroDetailEntity
+import cl.awakelab.sprint7.databinding.FragmentSuperHeroDetailBinding
+import coil.load
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +25,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SuperHeroDetailFragment : Fragment() {
+
+    lateinit var binding: FragmentSuperHeroDetailBinding
+    private val superHeroesViewModel: SuperHeroesViewModel by activityViewModels()
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,8 +45,44 @@ class SuperHeroDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_super_hero_detail, container, false)
+        binding = FragmentSuperHeroDetailBinding.inflate(LayoutInflater.from(activity))
+        initData()
+        return binding.root
+    }
+
+    private fun initData() {
+        superHeroesViewModel.getSuperheroDetail(param1!!.toInt(), requireContext())
+        lateinit var detail: SuperHeroDetailEntity
+        superHeroesViewModel.detailLiveData(param1!!.toInt()).observe(viewLifecycleOwner) {
+            if(it!=null) {
+                detail = it
+                Log.d("Into live data", "Objeto: ${it}")
+                binding.textViewDetailName.text = it.name
+                binding.textViewColor.text = it.color
+                binding.textViewDetailOrigin.text = it.origin
+                binding.textViewDetailPower.text = it.power
+                binding.textViewDetailCreationYear.text = it.creationYear.toString()
+                binding.imageView.load(it.imageUrl)
+                if(it.translation) {
+                    binding.textViewTranslation.text = R.string.translation_true.toString()
+                } else {
+                    binding.textViewTranslation.text = R.string.translation_false.toString()
+                }
+            }
+        }
+        binding.buttonVote.setOnClickListener {
+            Log.d("Button vote", "Button pressed")
+            val intent = Intent(Intent.ACTION_SENDTO)
+            val mail = arrayOf("comicconchile@hotmail.com")
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, mail)
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Votación ${detail.name} ")
+            intent.putExtra(Intent.EXTRA_TEXT, "Hola\n" +
+                    "Quiero que el siguiente super héroe ${detail.name} aparezca, en la nueva edición de\n" +
+                    "biografías animadas.\n" +
+                    "Número contacto: +56_________\n" +
+                    "Gracias.")
+        }
     }
 
     companion object {
--- app/src/main/java/cl/awakelab/sprint7/presentation/SuperHeroesViewModel.kt
+++ app/src/main/java/cl/awakelab/sprint7/presentation/SuperHeroesViewModel.kt
@@ -1,4 +1,34 @@
 package cl.awakelab.sprint7.presentation
 
-class SuperHeroesViewModel {
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import cl.awakelab.sprint7.data.Repository
+import cl.awakelab.sprint7.data.local.SuperHeroesDatabase
+import cl.awakelab.sprint7.data.remote.ApiRetrofit
+import kotlinx.coroutines.launch
+
+class SuperHeroesViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: Repository
+
+    fun superHeroesLiveData() = repository.getSuperHeroesEntity()
+
+    fun detailLiveData(id: Int) = repository.getSuperHeroDetailEntity(id)
+
+    init {
+        val api = ApiRetrofit.getRetrofitInfo()
+        val superHeroesDatabase = SuperHeroesDatabase.getDatabase(application).getSuperHeroesDao()
+        repository = Repository(api, superHeroesDatabase)
+    }
+
+    fun getAllSuperheroes(context: Context) = viewModelScope.launch {
+        repository.getSuperHeroes(context)
+    }
+
+    fun getSuperheroDetail(id: Int, context: Context) = viewModelScope.launch {
+        repository.getSuperHeroDetail(id, context)
+    }
+
 }
